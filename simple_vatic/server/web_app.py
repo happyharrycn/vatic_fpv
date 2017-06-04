1513d587620c27860b84952df25e9a0f589ef84b
happyharrycn <yli440@gatech.edu>
2017-06-03 20:51:46 -0400

    Simple task manager using TinyDB

--- simple_vatic/server/web_app.py
+++ simple_vatic/server/web_app.py
@@ -1,99 +1,116 @@
+"""A simple web server for video annotation"""
+
+# parsing args
+import argparse
+
 # encoding / decoding
 import json
 
 # time / logging
-import time
-
+#import time
 #import logging
 #import traceback
 
 # flask
 import flask
-import werkzeug
 import tornado.wsgi
 import tornado.httpserver
 
 # database
 from tinydb import TinyDB, Query
 
-# misc
-import argparse
-import os
-
 # Obtain the flask app object
-app = flask.Flask(__name__)
-
-# Instantiate a json database
-db = TinyDB('db.json')
+app = flask.Flask(__name__) # pylint: disable=invalid-name
 
 def load_annotation_tasks():
     """
     Wrapper for loading annotations
     """
-    # dummy data for testing
-    db.insert({
-            'id'         : 1,
-            'url'            : 'http://vjs.zencdn.net/v/oceans.mp4',
-            # tags for annotation
-            'named'          : False, 
-            'occluded'       : False,
-            'trimmed'        : False,
-            # link to untrimmed full video
-            'video_src'      : 'oceans',
-            'src_start_time' : 0,
-            'src_end_time'   : 0,
-            # annotations : action boundary
-            'start_time'     : -1.0,
-            'end_time'       : -1.0,
-            # annotations : action names
-            'action_verb'    : '',
-            'action_noun'    : []
-    })
-    
-    # returns a list of all dicts in the database
-    return db.all()
+
+    # Example item in the db
+    # {
+    #         'id'         : 1,
+    #         'url'            : 'http://vjs.zencdn.net/v/oceans.mp4',
+    #         # tags for annotation
+    #         'named'          : False,
+    #         'occluded'       : False,
+    #         'trimmed'        : False,
+    #         # link to untrimmed full video
+    #         'video_src'      : 'oceans',
+    #         'src_start_time' : 0,
+    #         'src_end_time'   : 0,
+    #         # annotations : action boundary
+    #         'start_time'     : -1.0,
+    #         'end_time'       : -1.0,
+    #         # annotations : action names
+    #         'action_verb'    : '',
+    #         'action_noun'    : []
+    # }
+
+    # Instantiate a json database
+    annotation_tasks = TinyDB('video_db.json')
+    # returns the database
+    return annotation_tasks
 
 def get_next_available_task(annotation_tasks, annotation_type):
     """
     Wrapper for getting a new task
-    """ 
-    # dummy code for getting a toy task
-    ant_task = annotation_tasks[0]
-    task = {
-        'id' : ant_task['id'],
-        'url' : ant_task['url'],
-        'annotation_type' : annotation_type
-    }
+    """
+    if annotation_type == 'name':
+        item = annotation_tasks.get(Query()['named'] == False)
+    else:
+        item = annotation_tasks.get((Query()['named'] == True)
+                                    & (Query()['trimmed'] == False))
+
+    # No task available
+    if item == None:
+        return None
+    else:
+        task = dict(item)
+        task['id'] = item.eid
 
     return task
 
-def update_task(annotation_tasks, video_id, annotation_type, json_res):
+def update_task(annotation_tasks, json_res):
     """
-    Wrapper for updating a existing task 
-    """     
+    Wrapper for updating a existing task. Quick and Dirty
+    """
+    # get annotation_type and video id
+    ant_type = json_res['annotation_type']
+    eid = json_res['id']
+
     # find the task to update
-    task = filter(lambda x: x['id'] == video_id, annotation_tasks)
-    
-    # update all keys in the task
-    for key, val in json_res.items():
-        task[key] = val
-    task[annotation_type] = True
-    
+    if ant_type == 'name':
+        # explicte update of the fields
+        annotation_tasks.update({'named' : True}, eids=[eid])
+        annotation_tasks.update({'occluded' : json_res['occluded']},
+                                eids=[eid])
+        annotation_tasks.update({'action_noun' : json_res['action_noun']},
+                                eids=[eid])
+        idx = annotation_tasks.update({'action_verb' : json_res['action_verb']},
+                                      eids=[eid])
+    else:
+        annotation_tasks.update({'trimmed' : True}, eids=[eid])
+        annotation_tasks.update({'start_time' : json_res['start_time']},
+                                eids=[eid])
+        idx = annotation_tasks.update({'end_time' : json_res['end_time']},
+                                      eids=[eid])
+
     # return true if task is updated, false if not
-    return task != None
+    return idx == eid
 
 @app.errorhandler(404)
 def not_found(error):
     """
     Default error handler for 404
     """
-    return flask.make_response(json.dumps({'error': 'Not found'}), 404)
+    return flask.make_response(json.dumps({'error': str(error)}), 404)
 
 @app.route('/get_task', methods=['POST'])
 def get_task():
     """
     Get a task from the server
-    A request is a json file with one field "annotation_type"
+    A request is a json file with a single field "annotation_type"
     """
     # an empty dict that holds the results
     ret = {}
@@ -102,7 +119,7 @@ def get_task():
     try:
         # make sure the content type is json
         request_type = flask.request.headers.get('Content-Type')
-        if not (request_type=='application/json'):
+        if request_type != 'application/json':
             raise ValueError('request type must be JSON')
 
         request_data = flask.request.get_data()
@@ -128,7 +145,7 @@ def get_task():
             if not ((ant_type == 'name') or (ant_type == 'trim')):
                 raise ValueError('unknown annotation_type')
 
-            # get next available task    
+            # get next available task
             task = get_next_available_task(app.annotation_tasks, ant_type)
 
             if not task:
@@ -158,7 +175,7 @@ def return_task():
     try:
         # make sure the content type is json
         request_type = flask.request.headers.get('Content-Type')
-        if not (request_type=='application/json'):
+        if request_type != 'application/json':
             raise ValueError('request type must be JSON')
 
         request_data = flask.request.get_data()
@@ -183,13 +200,12 @@ def return_task():
         else:
             # more sanity check
             ant_type = json_file['annotation_type']
-            video_id = json_file['id']
 
             if not ((ant_type == 'name') or (ant_type == 'trim')):
                 raise ValueError('unknown annotation_type')
 
-            # get next available task    
-            flag = update_task(app.annotation_tasks, video_id, ant_type, json_file)
+            # get next available task
+            flag = update_task(app.annotation_tasks, json_file)
 
             if not flag:
                 raise ValueError('can not update the task. Please re-try.')
@@ -214,30 +230,25 @@ def parse_args():
     parser.add_argument('--port', dest='port',
                         help='which port to serve content on',
                         default=5050, type=int)
-    
     args = parser.parse_args()
     return args
 
-
-def start_from_terminal(app):
+def start_from_terminal():
     """
     entry of the main function
     """
     # parse params
     args = parse_args()
-
     # load annotation tasks
     app.annotation_tasks = load_annotation_tasks()
-    
-    # start web server using tornado       
+    # start web server using tornado
     server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
     server.bind(args.port)
     # set up one server
     server.start(1)
-    print("Tornado server starting on port {}".format(args.port))
+    print "Tornado server starting on port {}".format(args.port)
     tornado.ioloop.IOLoop.current().start()
 
-
 if __name__ == '__main__':
-    start_from_terminal(app)
+    start_from_terminal()
 
